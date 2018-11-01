9773050b49beeb0a4a9da5d3c80a0f3fcf205809
Daniel Beato <dbeato@tntmax.com>
2018-11-01 16:03:14 -0400

    Updated Install Chocolatey Local PS

--- Chocolatey/Install-Chocolatey-Local.ps1
+++ Chocolatey/Install-Chocolatey-Local.ps1
@@ -1,1 +1,2 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install adobereader adobeair googlechrome 7zip.install jre8 firefox -y

4ff155cb448b526be8dbd6986e1d3b558074e122
unknown <danielbea@gmail.com>
2018-11-14 22:36:57 -0500

    Added Comodo RMM Linux Agent install Help

--- Linux/Comodo-RMM-Agent-install.sh
+++ Linux/Comodo-RMM-Agent-install.sh
@@ -1,3 +1,5 @@
 curl https://name-of-msp.cmdm.comodo.com/enroll/linux/run/token/e7a271c684525ac6c1cade645507058911 --output itsm_7XrPQwon_installer.run
 sudo chmod +x itsm_7XrPQwon_installer
 sudo ./itsm_7XrPQwon_installer
+#If you cannot run the installer due to a Libcurl issue run the following command
+sudo apt-get install libcurl4-openssl-dev

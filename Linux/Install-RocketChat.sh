e748935fda7fd0f58110269346b6e18e84067d02
Daniel Beato <danielbea@gmail.com>
2018-11-21 22:07:20 -0500

    Added RocketChat installer

---
+++ Linux/Install-RocketChat.sh
@@ -0,0 +1,4 @@
+sudo apt install snapd
+sudo snap install rocketchat-server
+# To configure go to  http://localhost:3000
+# More information here: https://rocket.chat/docs/installation/manual-installation/ubuntu/snaps/

ec52d87726d49a7ffcbec7c8ed31f37bc7d90134
Daniel Beato <dbeato@tntmax.com>
2018-11-26 12:28:49 -0500

    Added Copy Office Shortcuts to Public Desktop

---
+++ Windows/Copy-Office-Shortcuts.ps1
@@ -0,0 +1,7 @@
+
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk" C:\Users\Public\Desktop -Force
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk" C:\Users\Public\Desktop -Force
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PowerPoint.lnk" C:\Users\Public\Desktop -Force
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Publisher.lnk" C:\Users\Public\Desktop -Force
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk" C:\Users\Public\Desktop -Force
+Copy-Item "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Access.lnk" C:\Users\Public\Desktop -Force

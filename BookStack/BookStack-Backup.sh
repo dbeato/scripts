4af96cccc65a224c57ba7a9dc68b7f971ea38c45
Daniel Beato <dbeato@tntmax.com>
2019-08-20 09:43:22 -0400

    Updateed Bookstack Backup Script

--- BookStack/BookStack-Backup.sh
+++ BookStack/BookStack-Backup.sh
@@ -1,7 +1,8 @@
 find /scripts/ -type f -iname '*.sql' -mtime +1 -delete
-USER=usernanme
-PASS="password"
+BKLOCATION: /sql-backups
+DBUSER=usernanme
+DBPASS="password"
 DB=bookstack
 DATE=$(date +%Y-%m-%d-%H-%M)
-mysqldump -u $USER -p$PASS $DB > backup-$DATE.sql
-aws s3 cp *.sql s3://bucket-name/ --endpoint-url=https://s3.wasabisys.com
+mysqldump -u $DBUSER -p$DBPASS $DB > $BKLOCATION/backup-$DATE.sql
+aws s3 cp $BKLOCATION s3://bucket-name/ --endpoint-url=https://s3.wasabisys.com

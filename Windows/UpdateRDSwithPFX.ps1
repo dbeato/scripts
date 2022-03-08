da9446865c9e24d5ef7525e037f91df3b1fa6c50
Daniel Beato <dbeato@tntmax.com>
2022-03-08 10:05:32 -0500

    RDS Certificate Scripts

---
+++ Windows/UpdateRDSwithPFX.ps1
@@ -0,0 +1,24 @@
+﻿param($result)
+
+set-alias ps64 "$env:C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe"
+
+ps64 -args $result -command {
+
+   $result = $args[0]
+   $RDCB = "server.domain.com"
+   $pfxpath = $result.ManagedItem.CertificatePath
+  
+
+   Import-Module RemoteDesktop
+   Import-Module RemoteDesktopServices
+
+   Set-RDCertificate -Role RDPublishing -ImportPath $pfxpath -ConnectionBroker $RDCB -Force
+
+   Set-RDCertificate -Role RDWebAcces -ImportPath $pfxpath -ConnectionBroker $RDCB -Force
+
+   Set-RDCertificate -Role RDGateway -ImportPath $pfxpath -ConnectionBroker $RDCB -Force
+
+   Set-RDCertificate -Role RDRedirector -ImportPath $pfxpath  -ConnectionBroker $RDCB -Force
+
+   Restart-Service TSGateway -Force -ErrorAction Stop
+}
---
+++ Windows/UpdateRDSwithPFX3.ps1
@@ -0,0 +1,8 @@
+﻿$pfxpath = "C:\Certs\SSL.pfx"
+$RDCB = "server.domain.com"
+Import-Module RemoteDesktop
+Set-RDCertificate -Role RDPublishing -ImportPath $pfxpath -ConnectionBroker $RDCB -Force
+Set-RDCertificate -Role RDWebAcces -ImportPath $pfxpath -ConnectionBroker $RDCB -Force
+Set-RDCertificate -Role RDGateway -ImportPath $pfxpath -ConnectionBroker $RDCB -Force
+Set-RDCertificate -Role RDRedirector -ImportPath $pfxpath  -ConnectionBroker $RDCB -Force
+Restart-Service TSGateway -Force -ErrorAction Stop

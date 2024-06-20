01b404c1dff0c1361b67ca286291e44edade8583
Daniel Beato <dbeato@tntmax.com>
2024-06-20 12:12:17 -0400

    Added QB Clear Script

---
+++ Quickbooks/Clear-QB-Cache.ps1
@@ -0,0 +1,5 @@
+Get-ChildItem "C:\ProgramData\Intuit\QuickBooks 201*\Components\QBUpdateCache" -Force | Remove-Item -Recurse -Force
+Get-ChildItem "C:\ProgramData\Intuit\QuickBooks 201*\Components\DownloadQB*\SPatch*.dat" -Force | Remove-Item -Recurse -Force
+Get-ChildItem "C:\ProgramData\Intuit\QuickBooks 201*\Components\DownloadQB*\SPatch*.dat" -Force | Remove-Item -Recurse -Force
+Get-ChildItem "C:\ProgramData\Intuit\QuickBooks Enterprise Solutions*\Components\QBUpdateCache" -Force | Remove-Item -Recurse -Force
+Get-ChildItem "C:\ProgramData\Intuit\QuickBooks Enterprise Solutions*\Components\DownloadQB*\SPatch*.dat" -Force | Remove-Item -Recurse -Force

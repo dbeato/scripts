8e8b3e5a3fe4a01d5388db86090d8e96f5c65ddf
Daniel Beato <dbeato@tntmax.com>
2024-08-20 15:37:27 -0400

    Added Script to fix Shorcut Spacing

---
+++ Windows/Fix-Shortcut-Spacing.bat
@@ -0,0 +1,3 @@
+reg add HKCU\"Control Panel\Desktop\WindowMetrics" /v IconSpacing         /d "-1125" /f
+reg add HKCU\"Control Panel\Desktop\WindowMetrics" /v IconVerticalSpacing /d "-1125" /f
+REM Make sure to reboot computer after the changes are done. 

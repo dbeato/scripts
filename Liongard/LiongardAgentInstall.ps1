8a65891fc2365ce38f60082a06e8e03fc59845ba
Daniel Beato <dbeato@tntmax.com>
2025-01-14 12:23:58 -0500

    Added Script to Install the Liongard Agent

---
+++ Liongard/LiongardAgentInstall.ps1
@@ -0,0 +1,24 @@
+$folderName = "scripts"
+$Path="C:\"+$folderName 
+if (!(Test-Path $Path)) 
+{ 
+New-Item -itemType Directory -Path enter_path -Name $FolderName 
+Write-host “Scripts Folder created.” 
+} 
+else 
+{ 
+write-host "Scripts Folder already exists and not created" 
+} 
+
+Set-Location $Path
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri https://agents.static.liongard.com/LiongardAgent-lts.msi -OutFile ./LiongardAgent-lts.msi
+Start-Sleep -Seconds 90
+
+msiexec /i LiongardAgent-lts.msi LIONGARDURL=server.app.liongard.com LIONGARDACCESSKEY="AccessKey" LIONGARDACCESSSECRET="Secret" LIONGARDENVIRONMENT="Environment" /qn
+
+Start-Sleep -Seconds 240
+Remove-Item '$Path\LiongardAgent-lts.msi'
+
+
+
